2939e9df54e9be50d0f2c386fe13bc4f08fe57a3
Prakriti <prakriteekhanal@gmail.com>
2020-03-05 20:09:20 +0545

    completed info

---
+++ 2_info.py
@@ -0,0 +1,7 @@
+#1. Write a program(age__10_years_later.py) that asks a user for his/her name and age
+# and then tells them how old they will be after 10 years.
+
+your_name = input("Enter your name: ")
+your_age = input("Enter your age: ")
+age_years_later = int(your_age) + 10
+print(f"Hello your name is {your_name} and you are {your_age} years old, also after 10 years your age will be {age_years_later} years old ")
